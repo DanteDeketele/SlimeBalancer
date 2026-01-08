4e47745c1ce6b10c000c2061d20a20c4b8f95bab
DanteDeketele <mrdeegames@gmail.com>
2026-01-08 13:19:08 +0100

    SkiGame

--- SlimeBalancer/Assets/Scripts/Games/SkiGame.cs
+++ SlimeBalancer/Assets/Scripts/Games/SkiGame.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 public class SkiGame : BaseGame
 {
     public Transform Player;
+    public GameObject Floor;
+    public float Speed = 0.5f;
+    Vector3 _floorDirection;
+    GameObject _floorClone;
+    float _distanceTravelled = 0f;
     public override void StartGame()
     {
+        _floorDirection = Floor.transform.forward;
+        _floorClone = Instantiate(Floor, Floor.transform.position + _floorDirection * 400f, Floor.transform.rotation);
         base.StartGame();
     }
 
@@ -12,6 +19,12 @@ public class SkiGame : BaseGame
     {
         Vector2 input = GameManager.InputManager.InputVector;
         Player.Translate(Vector3.right * input.x * Time.deltaTime * 5f);
+
+        Speed += Time.deltaTime * 0.01f;
+
+        _distanceTravelled -= Speed * Time.deltaTime;
+        Floor.transform.position = _floorDirection * (_distanceTravelled % 400);
+        _floorClone.transform.position = _floorDirection * ((_distanceTravelled + 200) % 400);
     }
 
     public override void EndGame()
